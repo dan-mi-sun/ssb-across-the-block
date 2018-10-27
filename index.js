70747e59f0d7b05874f741362ff2d6c42c34af76
Dan Hassan <dh@blockades.org>
2018-10-27 12:09:10 +1100

    pruning results with sbot side map

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var pull = require('pull-stream')
 
 connection(function (error, server) {
   if (error) console.log(`This is the error: ${error}`)
+
   var opts = {
     reverse: true,
     query: [
@@ -14,20 +15,28 @@ connection(function (error, server) {
             }
           }
         }
+      }, {
+        $map: {
+          author: ['value', 'author'],
+          text: ['value', 'content', 'text'],
+          timestamp: ['value', 'timestamp'],
+        }
       }
     ]
   }
 
   var onDone = function (error, results) {
-    results.forEach(msg => {
-      console.log(msg.value.content)
+    results.forEach(result => {
+      console.log(result)
       console.log('----')
     })
+
+    console.log(results.length)
     server.close()
   }
   pull(
-    server.query.read(opts),
-    pull.take(10), 
-    pull.collect(onDone)
+    server.query.read(opts), // the source
+    pull.collect(onDone) // the sink, onDone is inMemory, pull.drain gives each result as it comes down the pipe use with console.log()
   )
 })
+
